a6e15e242f1ab1654e362981e725b654812dec16
masum691 <masumhu691@gmail.com>
2021-09-11 09:41:28 +0600

    object ts

---
+++ object.ts
@@ -0,0 +1,14 @@
+
+const city: string = 'Mymensingh';
+const fruits: string[] = ['apple', 'mango', 'banana'];
+type Person = {name:string, age:number, isGoodPerson: boolean}
+const student: Person = {
+    name: 'Masum hossain',
+    age: 21,
+    isGoodPerson: true
+}
+const employee: Person = {
+    name: 'Nasrin',
+    age: 30,
+    isGoodPerson: true
+}
