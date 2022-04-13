52e9515bc90ab0c9e6df369e8d107935f1a7a953
masumwbdev <masumwbd@gmail.com>
2022-04-14 00:17:56 +0600

    tuples

---
+++ js/tuples.js
@@ -0,0 +1,4 @@
+"use strict";
+let myTuple;
+myTuple = [665, true, "Hi My name is tuple"];
+console.log(myTuple);
---
+++ tuples.ts
@@ -0,0 +1,3 @@
+let myTuple: [number, boolean, string];
+myTuple = [665, true, "Hi My name is tuple"];
+console.log(myTuple);
